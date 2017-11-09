5065f0d5a60e86be4eea6b817adbe512a36f78b8
Daniel Warehime <daniel37@umbc.edu>
2017-11-09 18:26:04 +0100

    trying to push to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
-# require 'open-uri'
-# require 'json'
+require 'open-uri'
+require 'json'
 
 # Ingredient.destroy_all
 
-# url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-# file = open(url).string
-# contents = JSON.parse(file)
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+file = open(url).string
+contents = JSON.parse(file)
 
-# contents["drinks"].each do |ingredient|
-#   Ingredient.create!(name: ingredient["strIngredient1"])
-# end
+contents["drinks"].each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+end
 
 
-  ##USE LINE BELOW TO ADD INDIVIDUAL INGREDIENTS BY CHOICE,
-  ##LINES ABOVE HAD ALREADY BEEN LOADED INTO DB.
+  #USE LINE BELOW TO ADD INDIVIDUAL INGREDIENTS BY CHOICE,
+  #LINES ABOVE HAD ALREADY BEEN LOADED INTO DB.
 
-  # Ingredient.create!(name: "Whatever you want")
+  Ingredient.create!(name: "Whatever you want")
