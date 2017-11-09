096eeada1b876430ec21fee7290aa77c562f45b7
Daniel Warehime <daniel37@umbc.edu>
2017-11-09 18:08:46 +0100

    commpleted the requirements for the cocktail list assignment of the 1st day

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# require 'open-uri'
+# require 'json'
+
+# Ingredient.destroy_all
+
+# url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# file = open(url).string
+# contents = JSON.parse(file)
+
+# contents["drinks"].each do |ingredient|
+#   Ingredient.create!(name: ingredient["strIngredient1"])
+# end
+
+
+  ##USE LINE BELOW TO ADD INDIVIDUAL INGREDIENTS BY CHOICE,
+  ##LINES ABOVE HAD ALREADY BEEN LOADED INTO DB.
+
+  # Ingredient.create!(name: "Whatever you want")
